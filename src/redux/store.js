e95cb73ea4bc7f8d9406ff45a639bca5630dc0da
Adil <dev@iadil.com>
2020-04-26 01:14:59 -0400

    Change store redux dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,12 @@ import rootReducer from "./reducers/rootReducer";
 import {applyMiddleware, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 import {addDateToLoadedTodos, changeLoadedTodosId} from "./middleware/loadedTodosMiddleware";
+const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+
 export default createStore(
 	rootReducer,
 	compose(
 		applyMiddleware(thunk, addDateToLoadedTodos, changeLoadedTodosId),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		devTools
 	)
 )
