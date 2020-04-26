688baabdfba1d199eff60e71ab4655c91bf25c16
Adil <dev@iadil.com>
2020-04-26 00:46:49 -0400

    Updates

---
+++ precache-manifest.a438d924c7a3c74c1ae766885b4fb996.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "093c9e7790c8cce447225d3681d6c77d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe3d9f75a73485a5b3d",
+    "url": "/static/css/main.f0e31a2e.chunk.css"
+  },
+  {
+    "revision": "28b439df2368255c0a11",
+    "url": "/static/js/2.c1b5a48d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c1b5a48d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe3d9f75a73485a5b3d",
+    "url": "/static/js/main.6873e17a.chunk.js"
+  },
+  {
+    "revision": "e3fca218d5e10e974d23",
+    "url": "/static/js/runtime-main.98efe16c.js"
+  },
+  {
+    "revision": "cb743d43ef5f7515e71847bc502221a8",
+    "url": "/static/media/logo.cb743d43.png"
+  }
+]);
