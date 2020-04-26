9db1a0d2da22abaceb872ce89a1bed4878fc8470
Adil <dev@iadil.com>
2020-04-26 01:22:57 -0400

    Fix store redux devtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import rootReducer from "./reducers/rootReducer";
 import {applyMiddleware, compose, createStore} from "redux";
 import thunk from "redux-thunk";
 import {addDateToLoadedTodos, changeLoadedTodosId} from "./middleware/loadedTodosMiddleware";
-const devTools = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() : null
+// dev tools middleware
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
 export default createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(thunk, addDateToLoadedTodos, changeLoadedTodosId),
-		devTools
+	composeSetup(
+		applyMiddleware(thunk, addDateToLoadedTodos, changeLoadedTodosId)
 	)
 )
